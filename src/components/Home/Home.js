a7a57bf71254cb7f5e5b49775b667ad3168b77d5
Kunal Hole <kunalhole538@gmail.com>
2023-09-17 20:19:25 +0530

    Bug Fixing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,9 @@
 import bgImg from "../../assets/profile-pic.png"
 import { motion } from "framer-motion"
 import { useTypewriter, Cursor } from "react-simple-typewriter"
+import { useNavigate } from "react-router-dom"
 import { SiTwitter, SiLinkedin, SiGithub, SiInstagram } from "react-icons/si"
+
 const Home = () => {
   const objects = {
     initial: {
@@ -13,6 +15,7 @@ const Home = () => {
       opacity: 1,
     },
   }
+  const navigate = useNavigate()
 
   const [text] = useTypewriter({
     words: [
@@ -33,16 +36,19 @@ const Home = () => {
       className="h-full flex flex-col-reverse  font-sans items-center justify-around  overflow-hidden bg-transparent  md:justify-between  md:w-3/4 md:mx-auto   md:flex-row"
     >
       <div className=" flex flex-col  items-center justify-start overflow-hidden  space-y-5 md:items-start md:w-3/5">
-        <h4 className="text-primary font-bold  text-[30px]  mb-1 md:mb-3 md:text-[35px]">
+        <h4 className="text-primary font-semibold  text-[30px]  mb-1 md:mb-3 md:text-[35px]">
           Hello, I'm Kunal Hole
         </h4>
-        <h2 className="text-primary text-[25px] font-semibold md:text-[30px]">
+        <h2 className="text-primary text-[25px] font-medium md:text-[30px]">
           <span className="text-rear">{text}</span>
           <Cursor />
         </h2>
         <div className="mt-2">
-          <button className="text-white px-4 py-2 text-base  bg-transparent border border-rear rounded-lg cursor-pointer hover:text-rear md:px-6 md:py-4 md:text-lg md:font-medium">
-            Resume
+          <button
+            onClick={navigate("/")}
+            className="text-white px-4 py-2 text-base  bg-transparent border border-rear rounded-lg cursor-pointer hover:text-rear md:px-6 md:py-4 md:text-lg md:font-medium"
+          >
+            Get In Touch
           </button>
         </div>
         <div className="flex mt-5">
